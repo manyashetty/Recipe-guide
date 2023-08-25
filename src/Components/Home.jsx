d59326d78431bfdbb6537a83becd31fe37796c96
manyashetty <m12shetty@gmail.com>
2023-08-25 22:14:43 +0530

    2-2

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,32 +6,10 @@ import Food10 from "../Images/Food10.jpg";
 import "bootstrap/dist/css/bootstrap.min.css"
 const Home = () => {
     return (
-        // <div className='container-fluid'>
-        //     <div id="carouselExampleFade" className="carousel slide carousel-fade">
-        //         <div className="carousel-inner">
-        //             <div className="carousel-item active">
-        //                 <img src={require('../Images/Food9.jpg')} className="d-block w-100 img-fluid" alt="..." />
-        //             </div>
-        //             <div className="carousel-item">
-        //                 <img src={require('../Images/Food7.jpg')} className="d-block w-100 img-fluid" alt="..." />
-        //             </div>
-        //             <div className="carousel-item">
-        //                 <img src={require('../Images/Food10.jpg')} className="d-block w-100 img-fluid" alt="..." />
-        //             </div>
-        //         </div>
-        //         <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-        //             <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        //             <span className="visually-hidden">Previous</span>
-        //         </button>
-        //         <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-        //             <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        //             <span className="visually-hidden">Next</span>
-        //         </button>
-        //     </div>
-        // </div>
         < >
         
          <div id="price">
+          <section>
     <Carousel>
       <Carousel.Item>
         <img
@@ -66,7 +44,81 @@ const Home = () => {
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
+    </section>
+    {/* JUst Text as intro for website */}
+   <section id="introText">
+        <div class="container">
+            <div class="text-center">
+            <h1>Recipies for the foodiessss</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse interdum erat et neque tincidunt volutpat. Cras eget augue id dui varius pretium. Cras posuere dolor risus. Pellentesque elementum ultricies quam, sit amet rhoncus nisl viverra in. Cras imperdiet nisi a euismod molestie. Ut a metus arcu. Pellentesque feugiat dictum erat. Nulla vehicula condimentum purus, quis imperdiet nisl ultricies eleifend. Nulla quis rhoncus risus. Ut vel vehicula urna.</p>
+            </div>
+        </div>
+
+    </section>
+    {/* <!--About--> */}
+    <section id="aboutUs" class="secPad">
+        <div class="container">
+            <div class="heading text-center">
+                {/* <!-- Heading --> */}
+                <h2>get started with minimal ingride=ientt recipies </h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+            </div>
+            <div class="row">
+                {/* <!-- item --> */}
+                <div class="col-md-4 text-center tileBox">
+                   <div class="txtHead"> <i class="fa fa-desktop"></i>
+                    <h3>plainDesign</h3></div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse interdum erat et neque tincidunt volutpat. Cras eget augue id dui varius pretium.</p>
+                </div>
+                {/* <!-- end: --> */}
+
+                {/* <!-- item --> */}
+                <div class="col-md-4 text-center tileBox">
+                    <div class="txtHead"><i class="fa fa-css3"></i>
+                    <h3>HTML5/CSS3 Dev</h3></div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse interdum erat et neque tincidunt volutpat. Cras eget augue id dui varius pretium.</p>
+                </div>
+                {/* <!-- end: --> */}
+
+                {/* <!-- item --> */}
+                <div class="col-md-4 text-center tileBox">
+                    <div class="txtHead"><i class="fa fa-lightbulb-o"></i>
+                    <h3>JavaScript jQuery</h3></div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse interdum erat et neque tincidunt volutpat. Cras eget augue id dui varius pretium.</p>
+                </div>
+                {/* <!-- end: --> */}
+            </div>
+        </div>
+    </section>
+    {/* <!--Quote--> */}
+    <section id="quote" class="bg-parlex">
+        <div class="parlex-back">
+            <div class="container secPad text-center">
+				<h2>If I asked people what they wanted, they would have said ‘Faster Horses’.</h2><h3>-Henry Ford</h3>
+            </div>
+            {/* <!--/.container--> */}
+        </div>
+    </section>
     </div>
+    {/* <!-- Footer --> */}
+			<footer id="footer">
+
+				{/* <!-- Icons --> */}
+					<ul class="actions">
+						<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="#" class="icon fa-google-plus"><span class="label">Google+</span></a></li>
+						<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+						<li><a href="#" class="icon fa-pinterest"><span class="label">Pinterest</span></a></li>
+						<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+					</ul>
+
+				{/* <!-- Menu --> */}
+					<ul class="menu">
+						<li>&copy; Untitled</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
+
+			</footer>
     </>
         );
 }
