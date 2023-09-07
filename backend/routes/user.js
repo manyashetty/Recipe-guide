9fe0b4efed3d5d2889e015d85845aa188489139d
manyashetty <m12shetty@gmail.com>
2023-09-07 11:15:51 +0530

    pulling

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,9 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import { UserModel } from "../models/Users.js";
 
 const router = express.Router();
-import { UserModel } from "../models/Users.js";
 
 router.post("/register", async (req, res) => {
   const { username, password } = req.body;
